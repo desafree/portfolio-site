17fb63dcd7cc6fdcfccc73355cfe770e02b0f84b
desafree <nicoladesanctis99@gmail.com>
2022-06-05 19:38:34 +0200

    all section done, missing responsive + fix

--- script.js
+++ script.js
@@ -57,3 +57,101 @@ ScrollTrigger.create({
   start: 'top 90%',
   end: 'bottom 90%',
 });
+
+// -moz-transform": "matrix(1, 0, 0, 1, -75, 0)"
+
+const getTransformXValue = (index, target) => {
+  var style = window.getComputedStyle(target);
+  var matrix = new WebKitCSSMatrix(style.transform);
+  let [translateX, translateY] = [matrix.m41, matrix.m42];
+  if (translateX == -112.5 && translateY == 0) {
+    console.log('1');
+    return '0%';
+  } else if (translateX == 0 && translateY == -18.375) {
+    console.log('1');
+    return '75%';
+  } else if (translateX == 112.5 && translateY == 0) {
+    console.log('1');
+    return '0%';
+  } else if (translateX == 0 && translateY == 18.375) {
+    console.log('1');
+    return '-75%';
+  }
+};
+
+const getTransformYValue = (index, target) => {
+  console.log('start');
+  var style = window.getComputedStyle(target);
+  var matrix = new WebKitCSSMatrix(style.transform);
+  let [translateX, translateY] = [matrix.m41, matrix.m42];
+  console.log(translateX, translateY);
+  if (translateX == -112.5 && translateY == 0) {
+    console.log('1');
+    return '-75%';
+  } else if (translateX == 0 && translateY == -18.375) {
+    console.log('1');
+    return '0%';
+  } else if (translateX == 112.5 && translateY == 0) {
+    console.log('1');
+    return '75%';
+  } else if (translateX == 0 && translateY == 18.375) {
+    console.log('1');
+    return '0%';
+  }
+};
+
+var socialRotation = gsap.timeline({ repeat: -1 });
+
+socialRotation.to(
+  '.social-route .item1',
+  {
+    x: getTransformXValue,
+    y: getTransformYValue,
+    duration: 2,
+    ease: 'linear',
+  },
+  '<'
+);
+socialRotation.to(
+  '.social-route .item2',
+  {
+    x: getTransformXValue,
+    y: getTransformYValue,
+    duration: 2,
+    ease: 'linear',
+  },
+  '<'
+);
+socialRotation.to(
+  '.social-route .item3',
+  {
+    x: getTransformXValue,
+    y: getTransformYValue,
+    duration: 2,
+    ease: 'linear',
+  },
+  '<'
+);
+socialRotation.to(
+  '.social-route .item4',
+  {
+    x: getTransformXValue,
+    y: getTransformYValue,
+    duration: 2,
+    ease: 'linear',
+  },
+  '<'
+);
+
+// socialRotation.play();
+
+// const routeContainer = document.querySelector('.social-route');
+// routeContainer.addEventListener('mouseenter', () => {
+//   console.log('enter');
+//   socialRotation.pause();
+// });
+
+// routeContainer.addEventListener('mouseleave', () => {
+//   console.log('leave');
+//   socialRotation.resume();
+// });
